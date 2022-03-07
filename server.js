163d914e48dd5f28565fb863334154e23e579237
Sebastian Semeniuc <xsebyyy@gmail.com>
2022-03-07 18:51:10 +0200

    changed origin

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://realtime-chat-32a39.firebaseapp.com/",
     methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
   },
 })
